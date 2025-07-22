73cbb1189cdd114c02db210c8179733a8990a0c4
Kundan <151555907+Kundan-Soni@users.noreply.github.com>
2025-07-22 09:11:17 +0530

    Update server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -37,3 +37,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/user", userRoutes);
